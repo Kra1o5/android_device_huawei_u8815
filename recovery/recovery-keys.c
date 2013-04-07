66d8963fdc88c41b08ed3ab5eae9f3ee371693a5
Kra1o5 <kra1o5@hotmail.com>
2013-04-07 12:08:21 +0300

    Update Key Layout

--- recovery/recovery-keys.c
+++ recovery/recovery-keys.c
@@ -23,12 +23,15 @@ int device_handle_key(int key_code, int visible) {
             case KEY_MENU:
             case KEY_VOLUMEDOWN:
                 return HIGHLIGHT_DOWN;
+                    
+            case KEY_VOLUMEUP:
+                return HIGHLIGHT_UP;
 
             case KEY_HOME:
             case KEY_POWER:
                 return SELECT_ITEM;
                 
-            case KEY_VOLUMEUP:
+            case KEY_330:
             case KEY_BACK:
                 if (!ui_root_menu) {
                     return GO_BACK;
